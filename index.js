2e376bd1100b7d7fca317c3e70d885fb12387acf
Anucha Petchagun <big@ooca.co>
2019-11-25 16:55:05 +0700

    veryfirst commit

---
+++ index.js
@@ -0,0 +1,41 @@
+module.exports.default = function(req, res, next) {
+    const headers = req.headers
+    const queueName = headers['x-cloudtasks-queuename']
+    const taskName  = headers['x-cloudtasks-taskname']
+    const taskRetryCount = headers['x-cloudtasks-taskretrycount']
+    const taskExecutionCount = headers['x-cloudtasks-taskexecutioncount']
+    const taskETA = headers['x-cloudtasks-tasketa']
+    const taskPreviousResponse = headers['x-cloudtasks-taskpreviousresponse']
+    const taskRetryReason = headers['x-cloudtasks-taskretryreason']
+
+    if(!queueName) {
+        return next()
+    }
+
+    req.task = {
+        queue: queueName,
+        name: taskName,
+        retryCount: taskRetryCount,
+        executionCount: taskExecutionCount,
+        eta: taskETA,
+        previousResponse: taskPreviousResponse,
+        retryReason: taskRetryReason,
+    }
+
+    req.task.rawBody = '';
+    req.setEncoding('utf8');
+
+    req.on('data', function(chunk) { 
+        req.task.rawBody += chunk;
+    });
+
+    req.on('end', function() {
+        try {
+            req.task.body = JSON.parse(req.task.rawBody)    
+        } catch(e) {
+            req.task.body = req.task.rawBody
+        } finally {
+            next()
+        }
+    });
+}
